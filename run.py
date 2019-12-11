2645d9906f6a7703b2c555a0ed65c9721b801c6a
i-sava <voynyliv@ukr.net>
2019-12-12 01:23:45 +0200

    add run.py

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
-# a route example
-@app.route("/")
-def home():
-    return "Hello, Jeffrey Box. U R Kool with a K."
-# the bottom of your flask app
-if __name__ == "_main__":
-    app.run(debug=True)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run()
