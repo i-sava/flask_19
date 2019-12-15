1fc88ebc3b57c9eaa416648cb61ffbb1186e7b4a
i-sava <voynyliv@ukr.net>
2019-12-15 22:53:38 +0200

    edit click command

--- commands.py
+++ commands.py
@@ -8,24 +8,26 @@ from models import User
 @with_appcontext
 def create_tables():
     db.create_all()
+    click.echo("tables created!!!")
     #print("create_all was maked")
 
 @click.command(name='create_admin')
 @with_appcontext
 def create_admin():
-    click.echo('Hello! Run command ok')
+    #click.echo('Hello! Run command ok')
     nickname = "superadmin"
     admin = True
     u = User(nickname=nickname, admin=admin)
-    print("superadmin created", u)
+    #print("superadmin created", u)
+    
     db.session.add(u)
-    db.session.commit() 
+    db.session.commit()
+    click.echo("superadmin created!!!")
 
 #Heroku $ flask create_all
 
-'''
+
 if __name__ == '__main__':
 	pass
-	db.create_all()
+	create_tables()
 	create_admin()
-'''
