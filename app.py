0e5e1630c5854888afcaeb21f602a399f53cf0ff
i-sava <voynyliv@ukr.net>
2019-12-15 19:18:08 +0200

    improve setting

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+# app.config.from_pyfile(config_file = 'config.py')
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
--- config.py
+++ config.py
@@ -5,6 +5,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                           'sqlite:///' + os.path.join(basedir, 'app.db')
-SECRET_KEY = os.environ.get('ECRET_KEY') or \
+SECRET_KEY = os.environ.get('SECRET_KEY') or \
                           'secretsecret'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+APP_SETTINGS = "config.DevelopmentConfig"
+
