cf7d666b3c716dbcfcb1c0b6bb146a3575dd465d
i-sava <voynyliv@ukr.net>
2019-12-15 17:51:00 +0200

    add file routes and edit config

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                          'sqlite:///' + os.path.join(basedir, 'app.db')
+SECRET_KEY = os.environ.get('ECRET_KEY') or \
+                          'secretsecret'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
